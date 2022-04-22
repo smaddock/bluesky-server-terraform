b1e7966ccb120b8577c9c2f8d64f27ffad4f315a
Shawn Maddock <smaddock@gocirrus.com>
2022-04-22 00:32:19 -0600

    fixing deb 11 image issues

--- templates/cloud-init.sh
+++ templates/cloud-init.sh
@@ -1,7 +1,31 @@
 #!/bin/bash
+set -exuo pipefail
+
+# Debian 11 (Bullseye) builds have a typo in the APT source list
+sed \
+    --expression="s|http://security.debian.org bullseye/updates|http://security.debian.org bullseye-security|g" \
+    --in-place \
+    /etc/apt/sources.list
 
 # DigitalOcean first-boot journal gets corrupted, create a new one
 systemctl restart systemd-journald.service
+
+# Upgrade and install packages
+apt-get update -qq
+DEBIAN_FRONTEND=noninteractive apt-get upgrade --assume-yes --with-new-pkgs > /dev/null
+DEBIAN_FRONTEND=noninteractive apt-get install --assume-yes caddy jq sqlite3 > /dev/null
+
+# install latest BlueSky Server release
+DL_URL=$(curl --silent "https://api.github.com/repos/smaddock/bluesky-server/releases/latest" |
+    jq --raw-output ".assets[0].browser_download_url")
+curl \
+  --location \
+  --output bluesky-server.deb \
+  --show-error \
+  --silent \
+  "$DL_URL"
+apt install ./bluesky-server.deb
+rm --force bluesky-server.deb
 
 # swap packaged Caddy binary with one bundled with CGI module
 curl \
@@ -16,21 +40,9 @@ chmod 755 /usr/bin/caddy.custom
 update-alternatives --install /usr/bin/caddy caddy /usr/bin/caddy.default 10
 update-alternatives --install /usr/bin/caddy caddy /usr/bin/caddy.custom 50
 rm --force caddy_linux_amd64_custom
+rm --force /etc/caddy/Caddyfile
+mv /etc/caddy/Caddyfile.bluesky /etc/caddy/Caddyfile
 chown caddy:caddy /etc/caddy/Caddyfile
-
-# install latest BlueSky Server release
-DL_URL=$(curl --silent "https://api.github.com/repos/smaddock/bluesky-server/releases/latest" |
-    jq --raw-output ".assets[0].browser_download_url")
-curl \
-  --location \
-  --output bluesky-server.deb \
-  --show-error \
-  --silent \
-  "$DL_URL"
-apt install ./bluesky-server.deb
-rm --force bluesky-server.deb
-
-# restart the web server
 systemctl restart caddy
 
 # update the SSH server
